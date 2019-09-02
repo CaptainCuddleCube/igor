fd913662ae690e9364e2aff07cb75f25582e14cb
Ashton Hudson <ashtonhudson1@gmail.com>
2019-09-03 00:30:24 +0200

    feat: Moving away from using extra config.
    - Using tags on instances to determin access.
    - Using a better structure when determining what to pass functions.
    - Allowing all functions to be configurable.

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ from botocore.exceptions import ClientError
 from typing import List, Dict
 import requests
 import os
+from random import uniform
 
 
 # A simple slack bot that allows a user easy access to start, stop, reboot and check the status of
@@ -42,112 +43,58 @@ def error_handler(func):
     return wrapper
 
 
-class InstanceGroups:
-    def __init__(self, instances):
-        self._instances = instances
-
-    def get_group(self, group_name):
-        return self._instances.get(group_name, [])
-
-
 class Auth:
-    def __init__(self, access_groups, oauth="", app_token=""):
-        self._oauth_token = oauth if oauth != "" else os.environ["OAUTH_TOKEN"]
-        self._app_token = app_token if app_token != "" else os.environ["SLACK_TOKEN"]
-        self._access_groups = access_groups
+    def __init__(self, token=None):
+        self._oauth_token = os.environ["OAUTH_TOKEN"]
+        self._app_token = os.environ["SLACK_TOKEN"]
+        # self._access_groups = access_groups
+        if token is not None:
+            self.validate_token(token)
 
     def validate_token(self, token):
-        print(token, self._app_token)
         if token != self._app_token:
             raise ValueError("Access Denied")
 
-    def get_resource_groups(self, channel):
-
-        if not set([channel]) & set(self._access_groups):
-            raise ValueError("Access Denied")
-        channel_resources = self._access_groups.get(channel, "")
-        return channel_resources
-
     def staple_oath_token(self, data):
         data["token"] = self._oauth_token
-        print(data)
         return data
 
 
-class Igor:
-    def __init__(self, auth, channel, user_name, instances):
-        self._auth = auth
-        self._channel = channel
-        self._user = user_name
-        self._instance_groups = instances
-        dry_run = {"dry-run": "dry_run"}
-        force = {"force": "force"}
-        self._commands = {
-            "list-instances": {
-                "sub-commands": {},
-                "exec": self._list_instances,
-                "alert": False,
-            },
-            "reboot": {
-                "sub-commands": dry_run,
-                "exec": self._reboot_instance,
-                "alert": True,
-            },
-            "start": {
-                "sub-commands": dry_run,
-                "exec": self._start_instance,
-                "alert": True,
-            },
-            "status": {
-                "sub-commands": dry_run,
-                "exec": self._instance_state,
-                "alert": True,
-            },
-            "stop": {
-                "sub-commands": {**dry_run, **force},
-                "exec": self._stop_instance,
-                "alert": True,
-            },
-        }
-
-    def do_this(self, message: str) -> str:
-        instruction = message.split()
-        command = self._parse_command(instruction[0])
-        instance_id = (
-            self._get_instance_id(instruction[1]) if len(instruction) > 1 else ""
-        )
-        kwargs = self._get_kwarg_subcommands(command, instruction[2:])
-        kwargs = {**kwargs, "instance_id": instance_id}
-        response = self._run_command(command, **kwargs)
-        if self._commands[command]["alert"]:
-            self._send_slack_message(message, response)
-        return response
-
-    def _send_slack_message(self, user_message, command_response):
-        message = f"""{self._user} told igor to "{user_message}".\n{command_response}"""
-        data = dict(channel=self._channel, pretty=1, text=message)
-        data = self._auth.staple_oath_token(data)
-        print(data)
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "application/json",
-        }
-        reponse = requests.post(
-            "https://slack.com/api/chat.postMessage", data=data, headers=headers
-        )
-        print(reponse.json())
-
-    def _run_command(self, command, **kwargs):
-        return self._commands[command]["exec"](**kwargs)
-
-    def _format_state_change(self, prev: str, curr: str) -> str:
-        if prev == curr:
-            return f"Instance state has not changed from: {curr}"
-        else:
-            return f"Changing instande state: {prev} --> {curr}"
-
+class AwsFunctions:
+    @staticmethod
     @error_handler
-    def _instance_state(self, instance_id: str, dry_run: bool = False, **kwargs) -> str:
+    def get_instance_names(channel):
+        client = boto3.client("ec2")
+        response = client.describe_instances(
+            Filters=[{"Name": f"tag:Channel_id", "Values": [channel]}]
+        )
+        instance_names = []
+        for i in response["Reservations"]:
+            if "Instances" in i:
+                for j in i["Instances"]:
+                    name = [tag["Value"] for tag in j["Tags"] if tag["Key"] == "Name"]
+                    name = name.pop() if len(name) else ""
+                    instance_names.append(name)
+        return instance_names
+
+    @staticmethod
+    @error_handler
+    def get_instance_id(channel, name):
+        client = boto3.client("ec2")
+        response = client.describe_instances(
+            Filters=[
+                {"Name": "tag:Channel_id", "Values": [channel]},
+                {"Name": "tag:Name", "Values": [name]},
+            ]
+        )
+        for i in response["Reservations"]:
+            if "Instances" in i:
+                for j in i["Instances"]:
+                    return j["InstanceId"]
+
+    @staticmethod
+    @error_handler
+    def instance_state(instance_id: str, dry_run: bool = False) -> str:
         client = boto3.client("ec2")
         response = client.describe_instance_status(
             InstanceIds=[instance_id], DryRun=dry_run
@@ -157,80 +104,168 @@ class Igor:
         else:
             return f'Instance state: {response["InstanceStatuses"][0]["InstanceState"]["Name"]}'
 
+    @staticmethod
     @error_handler
-    def _start_instance(self, instance_id: str, dry_run: bool = False, **kwargs) -> str:
+    def start_instance(instance_id: str, dry_run: bool = False) -> str:
         client = boto3.client("ec2")
         response = client.start_instances(InstanceIds=[instance_id], DryRun=dry_run)
-        return self._format_state_change(
-            response["StartingInstances"][0]["PreviousState"]["Name"],
-            response["StartingInstances"][0]["CurrentState"]["Name"],
+        return AwsFunctions._format_state_change(
+            response["StartingInstances"], response["StartingInstances"]
         )
 
+    @staticmethod
     @error_handler
-    def _stop_instance(
-        self, instance_id: str, dry_run: bool = False, force: bool = False, **kwargs
+    def stop_instance(
+        instance_id: str, dry_run: bool = False, force: bool = False
     ) -> str:
         client = boto3.client("ec2")
         response = client.stop_instances(
             InstanceIds=[instance_id], DryRun=dry_run, Force=force
         )
-        return self._format_state_change(
-            response["StoppingInstances"][0]["PreviousState"]["Name"],
-            response["StoppingInstances"][0]["CurrentState"]["Name"],
+        return AwsFunctions._format_state_change(
+            response["StoppingInstances"], response["StoppingInstances"]
         )
 
+    @staticmethod
     @error_handler
-    def _reboot_instance(
-        self, instance_id: str, dry_run: bool = False, **kwargs
-    ) -> str:
+    def reboot_instance(instance_id: str, dry_run: bool = False) -> str:
         client = boto3.client("ec2")
         client.reboot_instances(InstanceIds=[instance_id], DryRun=dry_run)
         return "Instance is rebooting"
 
-    def _get_resources(self):
-        resource_group = self._auth.get_resource_groups(self._channel)
-        return self._instance_groups.get_group(resource_group)
+    @staticmethod
+    def _format_state_change(prev: str, curr: str) -> str:
+        prev = prev[0]["PreviousState"]["Name"]
+        curr = curr[0]["CurrentState"]["Name"]
+        if prev == curr:
+            return f"Instance state has not changed from: {curr}"
+        else:
+            return f"Instance changing: {prev} --> {curr}"
 
-    def _get_instance_id(self, instance_name: str) -> str:
-        instance_id = {
-            i["id"] for i in self._get_resources() if instance_name == i["name"]
+
+class Igor:
+    def __init__(self, channel, user_name, token):
+        self._auth = Auth(token)
+        self._channel = channel
+        self._user = user_name
+        self._exec_function_group = {"AwsFunctions": AwsFunctions}
+        self._peon_quotes = [
+            "No time for play.",
+            "Me not that kind of orc!",
+            "Okie dokie.",
+            "Work, work.",
+            "Why you poking me again?",
+            "Froedrick!",
+            "I've got no body, nobody's got me. Hachachacha.",
+        ]
+        self._commands = {
+            "list-instances": {
+                "sub-commands": [],
+                "requires": ["channel"],
+                "exec": {
+                    "function_group": "AwsFunctions",
+                    "function": "get_instance_names",
+                },
+                "alert": False,
+            },
+            "reboot": {
+                "sub-commands": ["dry_run"],
+                "requires": ["instance_id"],
+                "exec": {
+                    "function_group": "AwsFunctions",
+                    "function": "reboot_instance",
+                },
+                "alert": True,
+            },
+            "start": {
+                "sub-commands": ["dry_run"],
+                "requires": ["instance_id"],
+                "exec": {
+                    "function_group": "AwsFunctions",
+                    "function": "start_instance",
+                },
+                "alert": True,
+            },
+            "status": {
+                "sub-commands": ["dry_run"],
+                "requires": ["instance_id"],
+                "exec": {
+                    "function_group": "AwsFunctions",
+                    "function": "instance_state",
+                },
+                "alert": True,
+            },
+            "stop": {
+                "sub-commands": ["dry_run", "force"],
+                "requires": ["instance_id"],
+                "exec": {"function_group": "AwsFunctions", "function": "stop_instance"},
+                "alert": True,
+            },
         }
-        if len(instance_id) == 0:
-            return ""
 
-        return list(instance_id)[0]
+    def do_this(self, message: str) -> str:
+        instruction = message.split()
+        command = self._parse_command(instruction[0])
+        instance_id = (
+            AwsFunctions.get_instance_id(self._channel, instruction[1])
+            if len(instruction) > 1
+            else ""
+        )
+        kwargs = self._get_kwarg_subcommands(command, instruction[2:])
+        kwargs = {**kwargs, **self._required_inputs(command, instance_id)}
+        response = self._run_command(command, **kwargs)
+        if self._commands[command]["alert"]:
+            self.send_slack_message(message, response)
+            print(response)
+            return self._peon_quotes[int(uniform(0, len(self._peon_quotes)))]
+        else:
+            return response
 
-    def _list_instances(self, **kwargs) -> str:
-        return ", ".join([f'{i["name"]}' for i in self._get_resources()])
+    def _required_inputs(self, command, instance_id):
+        requirements = self._commands[command]["requires"]
+        switch = {"channel": self._channel, "instance_id": instance_id}
+        return {name: switch[name] for name in requirements}
+
+    def send_slack_message(self, user_message, command_response):
+        message = f"""{self._user} told igor to "{user_message}".\n{command_response}"""
+        data = dict(channel=self._channel, pretty=1, text=message)
+        data = self._auth.staple_oath_token(data)
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json",
+        }
+        requests.post(
+            "https://slack.com/api/chat.postMessage", data=data, headers=headers
+        )
+
+    def _run_command(self, command, **kwargs):
+        func_group = self._exec_function_group[
+            self._commands[command]["exec"]["function_group"]
+        ]
+        func = getattr(func_group, self._commands[command]["exec"]["function"])
+        return func(**kwargs)
 
     def _parse_command(self, command: str) -> str:
         if command not in self._commands:
             raise ValueError("Invalid command")
         return command
 
-    def _get_kwarg_subcommands(self, command, possible_subcommands: List) -> Dict:
+    def _get_kwarg_subcommands(self, command, provided_subcommands: List) -> Dict:
         subcommands = self._commands[command]["sub-commands"]
         # filter for commands that are valid and inject our own valid value.
         return {
             subcommands[i]: True
-            for i in list(set(possible_subcommands) & set(subcommands))
+            for i in list(set(provided_subcommands) & set(subcommands))
         }
 
 
 def lambda_handler(event, context):
     if event["command"] == "/igor":
-        with open("access_groups.json", "r") as file:
-            access_groups = json.load(file)
-            auth = Auth(access_groups)
-        with open("instance_groups.json", "r") as file:
-            instance_groups = InstanceGroups(json.load(file))
-
         message = event["text"]
         user = event["user_name"]
         channel = event["channel_id"]
         token = event["token"]
-        auth.validate_token(token)
-        igor = Igor(auth, channel, user, instance_groups)
+        igor = Igor(channel, user, token)
         return igor.do_this(message)
     else:
         return f"Command {event['command']} is unknown"
@@ -240,10 +275,9 @@ if __name__ == "__main__":
     event = {
         "token": "test-token",
         "command": "/igor",
-        "text": "list-instances instance",
+        "text": "stop Test-instance",
         "user_name": "test-user",
         "channel_id": "channel1",
-        "token": "test-token",
     }
 
     print(lambda_handler(event, {}))
